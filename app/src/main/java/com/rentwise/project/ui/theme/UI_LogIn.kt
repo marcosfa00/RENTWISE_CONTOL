4598328bd0ee681d7b7ab4e7144401a7f9080d15
Marcos Fernández Avendaño <113597220+marcosfa00@users.noreply.github.com>
2024-01-30 12:40:53 +0100

    to update

--- app/src/main/java/com/rentwise/project/ui/theme/UI_LogIn.kt
+++ app/src/main/java/com/rentwise/project/ui/theme/UI_LogIn.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
